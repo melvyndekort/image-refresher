eb26649c45925f0140d3b1e17a1fff41f54b2383
Melvyn de Kort <melvyn@mdekort.nl>
2023-02-21 17:15:51 +0100

    Add scheduling and slack notification

--- image-refresher.py
+++ image-refresher.py
@@ -3,13 +3,25 @@
 import docker
 import os
 import re
+import schedule
 
-client = docker.from_env()
+from slack_sdk.webhook import WebhookClient
 
-for key, value in os.environ.items():
-  if key.startswith('IMAGE'):
-    oldimage = client.images.get(value)
-    newimage = client.images.pull(value)
-    if newimage.id != oldimage.id:
-      client.images.remove(oldimage.id)
-      print('Image {} refreshed'.format(value))
+def refresh():
+  client = docker.from_env()
+  webhook = WebhookClient(os.environ['SLACK_WEBHOOK_URL'])
+
+  for key, value in os.environ.items():
+    if key.startswith('IMAGE'):
+      oldimage = client.images.get(value)
+      newimage = client.images.pull(value)
+      if newimage.id != oldimage.id:
+        client.images.remove(oldimage.id)
+        
+        response = webhook.send(text='Image {} refreshed'.format(value))
+        assert response.status_code == 200
+        assert response.body == "ok"
+  
+  client.close()
+
+schedule.every(2).hours.at(':15').do(refresh)
