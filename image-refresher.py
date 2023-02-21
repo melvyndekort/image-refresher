ba11a1512681938d13712e7837f75d7dff4b900b
Melvyn de Kort <m.de.kort@portbase.com>
2023-02-21 17:26:22 +0100

    Implement loop

--- image-refresher.py
+++ image-refresher.py
@@ -4,9 +4,11 @@ import docker
 import os
 import re
 import schedule
+import time
 
 from slack_sdk.webhook import WebhookClient
 
+# Function setup
 def refresh():
   client = docker.from_env()
   webhook = WebhookClient(os.environ['SLACK_WEBHOOK_URL'])
@@ -24,4 +26,11 @@ def refresh():
   
   client.close()
 
+# Task scheduling
+# 15 minutes after every 2 hours refresh() is called.
 schedule.every(2).hours.at(':15').do(refresh)
+
+# Loop so that the scheduling task keeps on running all time.
+while True:
+	schedule.run_pending()
+	time.sleep(1)
